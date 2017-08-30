46449cce22ec7eb6d4896ef50c7c592f68395902
kees1869 <kees1869@gmail.com>
2017-08-30 20:01:05 +0200

    Extending the Pickup Class

---
+++ Source/BatteryCollector/BatteryPickup.cpp
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BatteryPickup.h"
+
+
+// Sets default values
+ABatteryPickup::ABatteryPickup()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	// PrimaryActorTick.bCanEverTick = false;
+	
+	GetMesh()->SetSimulatePhysics(true);
+
+}
