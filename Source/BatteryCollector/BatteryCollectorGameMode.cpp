6f5a877160fc2a823f9fe65dd5d30adf90768d23
kees1869 <kees1869@gmail.com>
2017-09-20 21:30:44 +0200

    Enabling UMG

--- Source/BatteryCollector/BatteryCollectorGameMode.cpp
+++ Source/BatteryCollector/BatteryCollectorGameMode.cpp
@@ -4,6 +4,7 @@
 #include "BatteryCollectorCharacter.h"
 #include "UObject/ConstructorHelpers.h"
 #include "Kismet/GameplayStatics.h"
+#include "Blueprint/UserWidget.h"
 
 ABatteryCollectorGameMode::ABatteryCollectorGameMode()
 {
@@ -21,6 +22,27 @@ ABatteryCollectorGameMode::ABatteryCollectorGameMode()
 	DecayRate = 0.01f;
 }
 
+void ABatteryCollectorGameMode::BeginPlay()
+{
+	Super::BeginPlay();
+
+	/** set the score to beat */
+	ABatteryCollectorCharacter* MyCharacter = Cast<ABatteryCollectorCharacter>(UGameplayStatics::GetPlayerPawn(this, 0));
+	if (MyCharacter)
+	{
+		PowerToWin = (MyCharacter->GetInitialPower()) * 1.25f;
+	}
+
+	if (HUDWidgetClass != nullptr)
+	{
+		CurrentWidget = CreateWidget<UUserWidget>(GetWorld(), HUDWidgetClass);
+		if (CurrentWidget != nullptr)
+		{
+			CurrentWidget->AddToViewport();
+		}
+	}
+}
+
 // Called every frame
 void ABatteryCollectorGameMode::Tick(float DeltaTime)
 {
@@ -28,7 +50,7 @@ void ABatteryCollectorGameMode::Tick(float DeltaTime)
 
 	/** check that we are using the battery collector character */
 	ABatteryCollectorCharacter* MyCharacter = Cast<ABatteryCollectorCharacter>(UGameplayStatics::GetPlayerPawn(this, 0));
-	if(MyCharacter)
+	if (MyCharacter)
 	{
 		/** if the character's power is positive */
 		if (MyCharacter->GetCurrentPower() > 0)
--- Source/BatteryCollector/BatteryCollectorGameMode.h
+++ Source/BatteryCollector/BatteryCollectorGameMode.h
@@ -17,8 +17,26 @@ public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+	/** return power needed to win - needed for the HUD */
+	UFUNCTION(BlueprintPure, Category = "Power")
+	float GetPowerToWin() const;
+
+	virtual void BeginPlay() override;
+
 protected:
-	/**the rate at which the character loses power */
-	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Power")
+	/** the rate at which the character loses power */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Power", meta = (BlueprintProtected = true))
 	float DecayRate;
+
+	/** the power needed to win the game */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Power", meta = (BlueprintProtected = true))
+	float PowerToWin;
+
+	/** the widget class to use for our HUD screen */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Power", meta = (BlueprintProtected = true))
+	TSubclassOf<class UUserWidget> HUDWidgetClass;
+
+	/** the instance of the HUD */
+	UPROPERTY()
+	class UUserWidget* CurrentWidget;
 };
